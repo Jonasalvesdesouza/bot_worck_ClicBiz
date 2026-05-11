086091ad35b99d2458834114615c38b497a19994
Jonas Alves de Souza <souza.jonas.a@gmail.com>
2026-05-11 13:09:31 -0300

    deleted file dbclient

--- src/config/env.js
+++ src/config/env.js
@@ -4,5 +4,5 @@ module.exports = {
   SESSION: process.env.SESSION || 'production',
   CSV_FILE: process.env.CSV_FILE || 'data/client.csv',
   CSV_SEPARATOR: process.env.CSV_SEPARATOR || ';',
-  DELAY: Number(process.env.DELAY_BETWEEN_MESSAGES) || 8000,
+  DELAY: Number(process.env.DELAY_BETWEEN_MESSAGES) || 60000,
 };
